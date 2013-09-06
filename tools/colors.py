6eb8a37835baaf252fd472c612c87358d066fe08
john pazarzis <jpazarzis@gmail.com>
2013-09-06 09:27:58 -0400

    adding colors and dbtools

---
+++ tools/colors.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+PINK = '\033[95m'
+LIGHT_RED = '\033[91m'
+RED = '\033[1;31m'
+LIGHT_GRAY = '\033[90m'
+GRAY = '\033[1;30m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+BLUE = '\033[94m'
+PINK = '\033[95m'
+CYAN = '\033[96m'
+ENDC = '\033[0m'
+
+HRED = '\033[1;41m'
+HGREEN = '\033[1;42m'
+HBROWN = '\033[1;43m'
+HBLUE = '\033[1;44m'
+HMAGENTA = '\033[1;45m'
+HCYAN = '\033[1;46m'
+HGRAY =  '\033[1;47m'
+HCRIMSON = '\033[1;48m'
+HNORMAL = '\033[1;m'
+
+
+
+
+if __name__ == '__main__':
+        print BLUE + 'BLUE... '
+        print GREEN + 'GREEN... '
+        print PINK + 'PINK... '
+        print RED + 'RED... '
+        print LIGHT_RED + 'RED... '
+        print YELLOW + 'YELLOW... '
+        print GRAY + 'GRAY... '
+        print LIGHT_GRAY + 'GRAY... '
+        print CYAN + 'CYAN... '
+        print HRED + 'HRED... '
+        print HGREEN + RED + 'CYAN... ' 
+        print HBROWN + 'HBROWN... ' 
+        print HBLUE + 'HBLUE... ' 
+        print HMAGENTA + 'HMAGENTA... ' 
+        print HCYAN + 'HCYAN... ' 
+        print HGRAY + 'HGRAY... ' 
+        print HCRIMSON + 'HCRIMSON... ' 
+        print ENDC + 'ENDC... '
+        print 'done'
+
---
+++ tools/dbtools.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+''' Simple data base access tool'''
+
+
+import MySQLdb as mdb
+import atexit
+
+db_connection = None
+db_host="localhost"
+db_user="john"
+db_db="test"
+
+def connect():
+    global db_connection
+    db_connection = mdb.connect(host=db_host,user=db_user,db=db_db)
+    atexit.register(close_connection)
+
+
+def connect_if_needed():
+    ''' connects if there is no connection so far'''
+    global db_connection
+    if not db_connection:
+        connect()
+
+def close_connection():
+    ''' closes the connection'''
+    global db_connection
+    if db_connection:
+        db_connection.close()
+        db_connection=None
+
+def execute_query(sql,*args):
+        ''' executes a sql statement returning each row as an object
+            with property names matching the columns requested in the sql '''
+        connect_if_needed()
+        cur = db_connection.cursor(mdb.cursors.DictCursor)
+        cur.execute(sql.format(*args))
+        for i in range(cur.rowcount):
+            yield type('QueryResults', (object,), cur.fetchone())
+
+if __name__ == '__main__':
+            print 'testing race results'
+            SQL_GET_RESULTS_FOR_RACE="select id, program_number, odds, finish_position, race_id from  race_starters where track_code = '{0}' and racing_date = '{1}' and race_number={2} and program_number != 'SCR'"
+            for c in execute_query(SQL_GET_RESULTS_FOR_RACE, 'AQU','20100101',1):
+                print c.odds, c.id, c.finish_position
+
+
+
