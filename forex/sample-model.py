f5d43fad8ed1626d5e607cfa778ad366e701e65e
john pazarzis <jpazarzis@gmail.com>
2013-09-23 15:20:00 -0400

    add random model

--- forex/sample-model.py
+++ forex/sample-model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from tick_feed import get_tick
-
+import random
 '''
 This program implements a sample model using the tick feed, simulating the
 behavior of a simple strategy. We can create more sophisticate models using
@@ -12,46 +12,72 @@ Model where we need to descibe our trading logic...
 class BuyTrade:
     def __init__(self, buy_price):
         self.buy_price = buy_price
+
+def random_model(tick,engine):
+    r = random.random()
+    if r >= 0.9 and engine.current_trade is None:
+        engine.current_trade = BuyTrade(tick.ask)
+        return None
+
+    if engine.current_trade and tick.bid > engine.current_trade.buy_price + 0.002:
+        print 'won', 'bought at: ', engine.current_trade.buy_price, 'sold at: ',tick.bid,
+        return_value = 1
+        engine.current_trade = None
+    elif engine.current_trade and tick.bid < engine.current_trade.buy_price - 0.002:
+        print 'lost', 'bought at: ', engine.current_trade.buy_price, 'sold at: ',tick.bid,
+        return_value = -1
+        engine.current_trade = None
+    else:
+        return_value = None
+
+    return return_value
+
+
+
+def trivial_model(tick,engine):
+    max_bid_so_far = max([t.bid for t in engine.ticks])
+
+    if tick.ask > max_bid_so_far and engine.current_trade is None:
+        engine.current_trade = BuyTrade(tick.bid)
+
+    if engine.current_trade and tick.bid >= engine.current_trade.buy_price + 0.00002:
+        return_value = 1
+        engine.current_trade = None
+    elif engine.current_trade and tick.bid <= engine.current_trade.buy_price - 0.00002:
+        return_value = -1
+        engine.current_trade = None
+    else:
+        return_value = None
+
+    return return_value
+
     
 
-class Model:
-    def __init__(self, window_size = 200):
+class ModelEngine:
+    def __init__(self, model, window_size = 200):
         self.window_size = window_size
         self.ticks = [] # the tick window
         self.current_trade = None
+        self.model = model
 
     def process(self, tick,index):
-        
-        return_value = None
-        
-        # if the tick window is complete let's see if this tick
-        # is creating a signal
-        if len(self.ticks) == self.window_size:
-            max_bid_so_far = max([t.bid for t in self.ticks])
+        if len(self.ticks) < self.window_size:
+            # we do not have enough ticks yet
+            self.ticks.append(tick)
+            return None
 
-            if tick.bid > max_bid_so_far and self.current_trade is None:
-                self.current_trade = BuyTrade(tick.bid)
-
-            if self.current_trade and tick.bid >= self.current_trade.buy_price + 0.00002:
-                return_value = 1
-                self.current_trade = None
-            elif self.current_trade and tick.bid <= self.current_trade.buy_price - 0.0001:
-                return_value = -1
-                self.current_trade = None
-
-        # if the tick window is full, let's remove the first tick
-        # so we can add the new one in the list    
-        if len(self.ticks) == self.window_size:
-            self.ticks.pop(0)
-
+        return_value = self.model(tick,self)
+        self.ticks.pop(0)
         self.ticks.append(tick)
-        return return_value 
+        return return_value
         
         
         
 
 if __name__ == '__main__':
-    model = Model(1000)
+    #model = ModelEngine(trivial_model,1000)
+    model = ModelEngine(random_model,1000)
+
     i = 0    
     wins, loses = 0,0
     for tick in get_tick('./historical-ticks/EUR_USD.csv'):
@@ -61,7 +87,7 @@ if __name__ == '__main__':
             continue
         if r == 1: wins += 1
         if r == -1: loses += 1
-        print wins, loses
+        print ' wins: {0}, losses: {1}'.format(wins, loses)
 
     
 
