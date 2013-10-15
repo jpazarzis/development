d6a1ed37ab0e7cae6419520259d7b1ae4b7a4cec
john pazarzis <jpazarzis@gmail.com>
2013-10-15 09:31:07 -0400

    adding the multithread flag to makefile

--- forex/model-simulator/cpp_source/test.cpp
+++ forex/model-simulator/cpp_source/test.cpp
@@ -1,66 +1,64 @@
 #include "std_include.h"
 #include "Statistics.h"
 #include <numeric>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <thread>
 using namespace std;
 
 
+void test_drawdown()
+{
+        vector<double> v;
+        v.push_back(100000);
+        v.push_back(99287);
+        v.push_back(99353);
+        v.push_back(99418);
+        v.push_back(99485);
+        v.push_back(99551);
+        v.push_back(99617);
+        v.push_back(99684);
+        v.push_back(99750);
+        v.push_back(99816);
+        v.push_back(99109);
+        v.push_back(99174);
+        v.push_back(99240);
+        v.push_back(99307);
+        v.push_back(99372);
+        v.push_back(99439);
+        v.push_back(99504);
+        v.push_back(99569);
+        v.push_back(99635);
+        v.push_back(98929);
+        v.push_back(99000);
+        v.push_back(99065);
+        v.push_back(99131);
+        cout <<max_drawdown(v) << endl;
+        
+}
 
+void task1(string msg)
+{
+    cout << "task1 says: " << msg << endl;
+}
+
+void test_thread()
+{
+
+}
 
 int main()
 {
-        vector<double> v;
 
-        /*
-        v.push_back(10.0);
-        v.push_back(0.5);
-        v.push_back(-100);
-        v.push_back(-120);
-        v.push_back(-180);
-        v.push_back(-151.5);
-        19
-        */
+     //Starting the thread and running the function at the same time as we
+    //continue running the rest the function (main()) we are already in.
+    thread t1(task1, "Hello");
 
-        v.push_back(1000+10.0);
-        v.push_back(1000+0.5);
-        v.push_back(1000-100);
-        v.push_back(1000-120);
-        v.push_back(1000-180);
-        v.push_back(1000-151.5);
-        0.18
-
-
-
-/*
-        v.push_back(1000.00); 
-        v.push_back(1020.45); 
-        v.push_back(1107.19); 
-        v.push_back(1157.57); 
-        v.push_back(1192.25); 
-        v.push_back(1152.39); 
-        v.push_back(1112.90); 
-        v.push_back(1046.51); 
-        v.push_back(904.68); 
-        v.push_back(812.73); 
-        v.push_back(924.00); 
-        v.push_back(911.29); 
-        v.push_back(890.49); 
-        v.push_back(981.40); 
-        v.push_back(1033.45); 
-        v.push_back(1051.42); 
-        v.push_back(1038.79); 
-        v.push_back(960.97); 
-        v.push_back(1031.13); 
-        v.push_back(1029.80); 
-        v.push_back(1039.31); 
-        v.push_back(1056.20); 
-        v.push_back(1026.91); 
-        v.push_back(1038.69); 
-        v.push_back(1058.01); 
-        v.push_back(0.5); */
- 
-        cout << max_drawdown(v) << endl;
-
-
+    //Forces the original thread and the new thread we split off to come back 
+    //and merge together. What this means is that our default program thread
+    //waits continually until the new thread (ti) finishes executing.
+    t1.join();
 }
 
 
