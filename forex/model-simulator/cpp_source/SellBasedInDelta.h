6068ffe505a22e60b917c94f888109c38da9a889
john pazarzis <jpazarzis@gmail.com>
2013-10-28 18:42:21 -0400

    fixing some code formating issues

--- forex/model-simulator/cpp_source/SellBasedInDelta.h
+++ forex/model-simulator/cpp_source/SellBasedInDelta.h
@@ -10,9 +10,9 @@
 // Date  : Oct. 23 2013
 //
 // Summary: 
+//  Simple model based in the high price of the hour and a specific minute
+//  when the signal will be orginated
 //    
-
-
 
 #include "Model.h"
 #include "CloneableDouble.h"
@@ -42,8 +42,6 @@ class SellBasedInDelta: public Model
                 add_optimizable_field(&_stop_loss);
                 add_optimizable_field(&_take_profit);
                 add_optimizable_field(&_expriration_minutes);
-
-                
          }
 
     public:
@@ -60,7 +58,6 @@ class SellBasedInDelta: public Model
             initialize_optimizable_fields();
         }
 
-
         void set_values(double minute, double delta, double sl, double pt, double expriration_minutes)
         {
             _minute_to_buy.read_from_double(minute);
@@ -70,11 +67,9 @@ class SellBasedInDelta: public Model
             _expriration_minutes.read_from_double(expriration_minutes);
         }
 
-
         virtual ~SellBasedInDelta()
         {
         }
-
 
         void calculate_fitness()
         {
@@ -84,28 +79,21 @@ class SellBasedInDelta: public Model
             for(register int i = 0; i < number_of_ticks; ++i)
                 process_tick(tp[i], orders, i );
 
-            FitnessStatistics fs = FitnessStatistics::make(orders);
-            _fitness_statistics = fs;
-            set_fitness(fs.fitness());
+            _fitness_statistics = FitnessStatistics::make(orders);;
+            set_fitness( _fitness_statistics.fitness());
 
             for(auto order_ptr: orders)
-            {
                 Order::release(order_ptr);
-            }
-            
         }
 
         void process_tick(const Tick& tick, std::vector<Order*>& orders, int current_tick_index)
         {
-            if((int)tick.timestamp().time_of_day().hours() != _current_hour)
-            {
+            if((int)tick.timestamp().time_of_day().hours() != _current_hour) {
                 _current_hour = (int)tick.timestamp().time_of_day().hours();
                 _open_price = tick.bid();
                 _triggered_for_current_hour = false;
                 _high_for_the_hour = tick.bid();
-            }
-            else
-            {
+            } else  {
                     if(tick.bid() > _high_for_the_hour)
                         _high_for_the_hour = tick.bid();
 
@@ -113,8 +101,7 @@ class SellBasedInDelta: public Model
                         return;
 
                     const double delta_in_pips = (_high_for_the_hour - _open_price) * 10000;
-                    if(delta_in_pips >= (double)_triggering_delta)
-                    {
+                    if(delta_in_pips >= (double)_triggering_delta) {
                         Order* pOrder = Order::make( SELL, (double)_stop_loss, (double)_take_profit, tick, (int) ((double)_expriration_minutes));
                         assert(NULL != pOrder);
                         orders.push_back(pOrder);
@@ -128,105 +115,46 @@ class SellBasedInDelta: public Model
         {
             using namespace std;
 
-            if((int)tick.timestamp().time_of_day().hours() != _current_hour)
-            {
+            if((int)tick.timestamp().time_of_day().hours() != _current_hour) {
                 _current_hour = (int)tick.timestamp().time_of_day().hours();
                 _open_price = tick.bid();
                 _triggered_for_current_hour = false;
                 _high_for_the_hour = tick.bid();
-            }
-            else
-            {
+            } 
+            else {
                     if(tick.bid() > _high_for_the_hour)
                         _high_for_the_hour = tick.bid();
 
-
                     if(_triggered_for_current_hour || (int) tick.timestamp().time_of_day().minutes() != (int)_minute_to_buy)
                         return;
-
-                    //const double delta_in_pips = (tick.bid - _open_price) * 10000;
                     const double delta_in_pips = (_high_for_the_hour - _open_price) * 10000;
-
                     if(delta_in_pips >= (double)_triggering_delta)
                     {
                         add_order(Order::make( SELL, (double)_stop_loss, (double)_take_profit, tick, (int) ((double)_expriration_minutes)));
+                        _triggered_for_current_hour = true;
                     }
-
-                    _triggered_for_current_hour = true;
             }
         }
-
 
         std::string get_full_description() const
         {
             std::string strg;
-
-            strg += sformat("id:", "%20s");
-            strg += sformat((int)_id,"%20d"); 
-            strg += "\n";
-
-
-            strg += sformat("minute to trade:", "%20s");
-            strg += sformat((int)_minute_to_buy,"%20d"); 
-            strg += "\n";
-
-            strg += sformat("expriration minutes:", "%20s");
-            strg += sformat((double)_expriration_minutes, "%20.5f"); 
-            strg += "\n";
-
-
-            strg += sformat("delta:", "%20s");
-            strg += sformat((double)_triggering_delta, "%20.5f"); 
-            strg += "\n";
-
-            strg += sformat("stop loss:", "%20s");
-            strg += sformat((double)_stop_loss, "%20.5f"); 
-            strg += "\n";
-
-            strg += sformat("profit take:", "%20s");
-            strg += sformat((double)_take_profit, "%20.5f"); 
-            strg += "\n";
-
-            strg += sformat("number of orders:", "%20s");
-            strg += sformat(orders_count(),"%20d"); 
-            strg += "\n";
-
-            strg += sformat("final balance:", "%20s");
-            strg += sformat(get_account_balance(), "%20.2f");
-            strg += "\n";
-
-            strg += sformat("final pnl:", "%20s");
-            strg += sformat(get_pnl(), "%20.2f");
-            strg += "\n";
-
-            strg += sformat("low:", "%20s");
-            strg += sformat(get_absolute_low(), "%20.2f");
-            strg += "\n";
-
-            strg += sformat("winning trades:", "%20s");
-            strg += sformat(get_winning_trades_count(), "%20d");
-            strg += "\n";
-
-            strg += sformat("losing trades:", "%20s");
-            strg += sformat(get_loosing_trades_count(), "%20d");
-            strg += "\n";
-
-            strg += sformat("expired trades total:", "%20s");
-            strg += sformat(get_expired_trades_count(), "%20d");
-            strg += "\n";
-
-            strg += sformat("winning expired count:", "%20s");
-            strg += sformat(get_winning_expired_trades_count(), "%20d");
-            strg += "\n";
-
-            strg += sformat("losing expired count:", "%20s");
-            strg += sformat(get_losing_expired_trades_count(), "%20d");
-            strg += "\n";
-
-            strg += sformat("max drawdown:", "%20s");
-            strg += sformat(get_max_drawdown(), "%20.5f");
-            strg += "\n";
-
+            strg += sformat("id:", "%20s"); strg += sformat((int)_id,"%20d"); strg += "\n";
+            strg += sformat("minute to trade:", "%20s"); strg += sformat((int)_minute_to_buy,"%20d"); strg += "\n";
+            strg += sformat("expriration minutes:", "%20s"); strg += sformat((double)_expriration_minutes, "%20.5f"); strg += "\n";
+            strg += sformat("delta:", "%20s"); strg += sformat((double)_triggering_delta, "%20.5f"); strg += "\n";
+            strg += sformat("stop loss:", "%20s"); strg += sformat((double)_stop_loss, "%20.5f"); strg += "\n";
+            strg += sformat("profit take:", "%20s"); strg += sformat((double)_take_profit, "%20.5f"); strg += "\n";
+            strg += sformat("number of orders:", "%20s"); strg += sformat(orders_count(),"%20d"); strg += "\n";
+            strg += sformat("final balance:", "%20s"); strg += sformat(get_account_balance(), "%20.2f"); strg += "\n";
+            strg += sformat("final pnl:", "%20s"); strg += sformat(get_pnl(), "%20.2f"); strg += "\n";
+            strg += sformat("low:", "%20s"); strg += sformat(get_absolute_low(), "%20.2f"); strg += "\n";
+            strg += sformat("winning trades:", "%20s"); strg += sformat(get_winning_trades_count(), "%20d"); strg += "\n";
+            strg += sformat("losing trades:", "%20s"); strg += sformat(get_loosing_trades_count(), "%20d"); strg += "\n";
+            strg += sformat("expired trades total:", "%20s"); strg += sformat(get_expired_trades_count(), "%20d"); strg += "\n";
+            strg += sformat("winning expired count:", "%20s"); strg += sformat(get_winning_expired_trades_count(), "%20d"); strg += "\n";
+            strg += sformat("losing expired count:", "%20s"); strg += sformat(get_losing_expired_trades_count(), "%20d"); strg += "\n";
+            strg += sformat("max drawdown:", "%20s"); strg += sformat(get_max_drawdown(), "%20.5f"); strg += "\n";
             return strg;
         }
 
@@ -234,36 +162,13 @@ class SellBasedInDelta: public Model
         std::string get_full_description2() const
         {
             std::string strg;
-
-            strg += sformat("id:", "%20s");
-            strg += sformat((int)_id,"%20d"); 
-            strg += "\n";
-
-
-            strg += sformat("minute to trade:", "%20s");
-            strg += sformat((int)_minute_to_buy,"%20d"); 
-            strg += "\n";
-
-            strg += sformat("expriration minutes:", "%20s");
-            strg += sformat((double)_expriration_minutes, "%20.5f"); 
-            strg += "\n";
-
-
-            strg += sformat("delta:", "%20s");
-            strg += sformat((double)_triggering_delta, "%20.5f"); 
-            strg += "\n";
-
-            strg += sformat("stop loss:", "%20s");
-            strg += sformat((double)_stop_loss, "%20.5f"); 
-            strg += "\n";
-
-            strg += sformat("profit take:", "%20s");
-            strg += sformat((double)_take_profit, "%20.5f"); 
-            strg += "\n";
-
+            strg += sformat("id:", "%20s"); strg += sformat((int)_id,"%20d"); strg += "\n";
+            strg += sformat("minute to trade:", "%20s"); strg += sformat((int)_minute_to_buy,"%20d"); strg += "\n";
+            strg += sformat("expriration minutes:", "%20s"); strg += sformat((double)_expriration_minutes, "%20.5f"); strg += "\n";
+            strg += sformat("delta:", "%20s"); strg += sformat((double)_triggering_delta, "%20.5f"); strg += "\n";
+            strg += sformat("stop loss:", "%20s"); strg += sformat((double)_stop_loss, "%20.5f"); strg += "\n";
+            strg += sformat("profit take:", "%20s"); strg += sformat((double)_take_profit, "%20.5f"); strg += "\n";
             strg += _fitness_statistics.get_full_description();
-
-
             return strg;
         }
 
