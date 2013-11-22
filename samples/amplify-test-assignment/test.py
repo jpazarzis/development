60169752a699fdac8e22d2ea1f8273ade5d25340
john pazarzis <jpazarzis@gmail.com>
2013-11-22 12:38:21 -0500

    changes to assesment test

--- samples/amplify-test-assignment/test.py
+++ samples/amplify-test-assignment/test.py
@@ -23,7 +23,7 @@ ls
 import sys
 import csv
 import xml.etree.cElementTree as ET
-
+import os.path
 
 def xml2obj(filename):
     '''
@@ -100,7 +100,12 @@ def csv2obj(filename):
         index += 1
         student.first_name = tokens[index]
         index += 1
-        student.student_grade = tokens[index]
+        print len(tokens) , index
+        if len(tokens) >= index+1:
+            print 'here'
+            student.student_grade = tokens[index]
+        else:
+            student.student_grade = ""
 
         if len(student.id) >0:
             classroom.student.append(student)
@@ -131,28 +136,81 @@ def obj2xml(obj, left_margin_count = 0):
         strg +='/>\n'
     return strg
 
-    
-    
+def obj2csv(obj):
+    def get_teachers(teachers):
+            strg = ''
+            if not teachers or len(teachers) == 0:
+                strg += ',,,,,,'
+            elif len(teachers) == 1:
+                strg += ','.join([teachers[0].id, teachers[0].last_name, teachers[0].first_name]) + ',,,,'
+            else:
+                strg += ','.join([teachers[0].id, teachers[0].last_name, teachers[0].first_name]) + ','
+                strg += ','.join([teachers[1].id, teachers[1].last_name, teachers[1].first_name]) + ','
+            return strg 
 
-def test_xml():
-    a = xml2obj('sample_data.xml')
-    print a.grade[0].id
-    for cr in a.grade[0].classroom:
-        print cr.id, cr.name
+    grade = obj.grade[0]
+    strg = 'classroom id, classroom_name, teacher_1_id, teacher_1_last_name,\
+    teacher_1_first_name, teacher_2_id, teacher_2_last_name,\
+    teacher_2_first_name, student_id, student_last_name, student_first_name,\
+    student_grade\n'
+    for classroom in grade.classroom:
+        students, teachers = None, None
+        if hasattr(classroom, 'student'):
+            students = classroom.student
+        if hasattr(classroom, 'teacher'):
+            teachers = classroom.teacher
+        strg += '{0},{1},'.format(classroom.id, classroom.name)
+        if students:
+                for student in classroom.student:
+                    strg += get_teachers(teachers)
+                    students = classroom.student    
+                    if len(students) == 0:
+                        strg += ',,'
+                    else :
+                        strg += ','.join([student.id, student.last_name, student.first_name])
+                        if hasattr(student,'student_grade'):
+                            strg += ',{0}'.format(student.student_grade)
+                    strg += '\n' 
 
+        if not students:     
+            strg += get_teachers(teachers) + ',,,' + '\n' 
+    return strg
+    
 
 if __name__ == '__main__':
 
-    a = csv2obj('sample_data.csv')
-    print obj2xml(a)
+    s = csv2obj('junk.csv')
+    print s
+    sys.exit(1)
 
+    if len(sys.argv) != 3:
+        print 'please provide in the command line the input and the output filenames'
+        sys.exit(1)
 
+    readers = {'.xml':xml2obj, '.csv': csv2obj}
+    writers = {'.xml':obj2xml, '.csv': obj2csv}
 
+    try:
+            input_filename = sys.argv[1]
+            output_filename = sys.argv[2]
 
+            input_extension = os.path.splitext(input_filename)[1]
+            output_extension = os.path.splitext(output_filename)[1]
 
+            if input_extension not in readers:
+                raise Exception("Extension: " + input_extension + " is not supported")
 
+            if output_extension not in writers:
+                raise Exception("Extension: " + output_extension + " is not supported")
 
+            if input_extension == output_extension:
+                raise Exception("Cannot convert to the same type.")
 
+            reader = readers[input_extension]
+            writer = writers[output_extension]
 
+            with open(output_filename, 'w') as f:
+                f.write(writer(reader(input_filename)))
 
-
+    except Exception as ex:
+        print 'Error:',ex
